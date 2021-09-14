20ff96b523dfe42720249b5e25301f1a8c5314a1
Bogdan Cebere <bogdan.cebere@gmail.com>
2021-09-14 16:33:12 +0100

    code review + cleanup

--- src/data/dm2livsim.py
+++ src/data/dm2livsim.py
@@ -4,8 +4,9 @@
 
 import pandas as pd
 
+
 def save_input_files(location, dm):
-    # params: 
+    # params:
     # :location: str - is the location where the input files will be stored
     # :dm: DataModule - is either UNOS or UKReg or U2U
 
@@ -19,9 +20,6 @@ def save_input_files(location, dm):
     # Replication (take 1) | DSA ID (take 1) | DSA ID (take 1) | Donor Arrival Time (years) | Donor ABO Blood Type | Organ ID | ...add additional features...
     organs = dm._test_processed[dm.o_cols]
     out = pd.DataFrame()
-
-    
-
 
     # DSA_AvgTimes.txt
     # -> not considered
@@ -41,7 +39,6 @@ def save_input_files(location, dm):
     # Patients.txt
     # Replication (take 1) | Patient ID | DSA ID (take 1) | DSA ID (take 1) | Patient Arrival Time (years) | Patient ABO | Patient Lab MELD | Patient HCC (0, 1) | Status1 (take 0) | Sodium Score | Inactive (0) | ...add additional features...
 
-
     # Patients_Accept.txt
     # -> not considered
 
@@ -55,15 +52,9 @@ def save_input_files(location, dm):
     # Step probability | Days survived post-tx | Group probability
     # TODO: check how Step prob. calculates days survived. Can we just fill in days survivied?
 
-
     # survivalcoefficients.txt
     # -> change with model
 
     # Waitlist_matchmeld.txt
 
     pass
-
-
-
-
-
